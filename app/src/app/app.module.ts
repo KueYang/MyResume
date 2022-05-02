58b5a15c1599932e34ddf72e5208517389387cb5
Kue Yang <kueyang12@gmail.com>
2022-05-02 15:25:11 -0400

    Added projects page. Reworked navigation.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,32 +1,30 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutComponent } from './components/about/about.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { EducationComponent } from './components/education/education.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { InterestsComponent } from './components/interests/interests.component';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShareModule } from './share/share.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutComponent,
-    ExperienceComponent,
-    EducationComponent,
-    SkillsComponent,
-    InterestsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    ReactiveFormsModule,
+
+    ToastrModule,
+    ModalModule.forRoot(),
     
-    FontAwesomeModule
+    ShareModule,
+
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
